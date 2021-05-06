eee7411c9ef597bd54dd4c061b66b6d4ac34a611
DESKTOP-1KI21TC\CNL-B3 <gksehdmgl@gmail.com>
2021-05-06 17:48:43 +0900

    [추가]sinc 함수 구현 및 figure 추가

---
+++ Util/math.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 17:30:31 2021
+
+@author: CNL-B3
+"""
+import math 
+import matplotlib.pyplot as plt
+import numpy as np
+
+def sinc(x):
+    if x == 0:
+        return 1
+    else:
+        return (math.sin(math.pi * x) / (math.pi * x))
+
+x = np.linspace(-5,5,1000)
+y = []
+
+for valueX in x:
+    y.append(sinc(valueX))
+
+
+plt.plot(x,y)
+
+plt.show()
