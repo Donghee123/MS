80d3294c2fdf301aa96f82f716c4f9d6f6bb49a8
HANDONGHEE <gksehdmgl@gmail.com>
2021-05-13 02:10:12 +0900

    [추가]시뮬레이션 결과 그래프 추가

--- RL Homework/mountainCar/mountaincar.py
+++ RL Homework/mountainCar/mountaincar.py
@@ -4,30 +4,16 @@ Created on Mon May 10 22:06:09 2021
 
 @author: Handonghee
 """
-
-#######################################################################
-# Copyright (C)                                                       #
-# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
-# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
-# Permission given to modify the code as long as you keep this        #
-# declaration at the top                                              #
-#######################################################################
-
 import numpy as np
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 from math import floor
+import math
 
-#######################################################################
-# Following are some utilities for tile coding from Rich.
-# To make each file self-contained, I copied them from
-# http://incompleteideas.net/tiles/tiles3.py-remove
-# with some naming convention changes
-#
-# Tile coding starts
 class IHT:
     "Structure to handle collisions"
     def __init__(self, size_val):
@@ -41,10 +27,9 @@ class IHT:
     def full(self):
         return len(self.dictionary) >= self.size
 
-    def get_index(self, obj, read_only=False):
-        d = self.dictionary
-        if obj in d:
-            return d[obj]
+    def get_index(self, obj, read_only=False): 
+        if obj in self.dictionary:
+            return self.dictionary[obj]
         elif read_only:
             return None
         size = self.size
@@ -54,13 +39,16 @@ class IHT:
             self.overfull_count += 1
             return hash(obj) % self.size
         else:
-            d[obj] = count
+            self.dictionary[obj] = count
             return count
 
 def hash_coords(coordinates, m, read_only=False):
-    if isinstance(m, IHT): return m.get_index(tuple(coordinates), read_only)
-    if isinstance(m, int): return hash(tuple(coordinates)) % m
-    if m is None: return coordinates
+    if isinstance(m, IHT):
+        return m.get_index(tuple(coordinates), read_only)
+    if isinstance(m, int): 
+        return hash(tuple(coordinates)) % m
+    if m is None: 
+        return coordinates
 
 def tiles(iht_or_size, num_tilings, floats, ints=None, read_only=False):
     """returns num-tilings tile indices corresponding to the floats and ints"""
@@ -81,39 +69,71 @@ def tiles(iht_or_size, num_tilings, floats, ints=None, read_only=False):
 # Tile coding ends
 #######################################################################
 
-# all possible actions
-ACTION_REVERSE = -1
-ACTION_ZERO = 0
-ACTION_FORWARD = 1
-# 선택할 수 있는 action을 왼쪽, 정지, 오른쪽
-ACTIONS = [ACTION_REVERSE, ACTION_ZERO, ACTION_FORWARD]
 
-# bound for position and velocity
+EPSILON = 0
+VELOCITY_ONESTEPSIZE = 0.001
+GRAVITY_ONESTEPSIZE = 0.0025
+REWARD = -1
+
+#선택가능한 액션 3개 정의 Left, None, Right
+ACTION_LEFT = -1
+ACTION_NONE = 0
+ACTION_RIGHT = 1
+
+# 선택할 수 있는 action을 왼쪽, 정지, 오른쪽
+ACTIONS = [ACTION_LEFT, ACTION_NONE, ACTION_RIGHT]
+
+# cos(MAP_LEFT) ~ cos(MAP_RIGHT) y값의 범위
+# MAP_LEFT ~ MAP_RIGHT X값의 범위
 
 #맵에서 왼쪽 끝
-POSITION_MIN = -1.2
+MAP_LEFT = -1.2
+
 #맵에서 오른쪽 끝 
-POSITION_MAX = 0.5
+MAP_RIGHT = 0.5
 
 #왼쪽으로 낼 수있는 최대 속력
-VELOCITY_MIN = -0.07
+VELOCITY_LEFT = -0.07
 
 #오른쪽으로 낼 수있는 최대 속력
-VELOCITY_MAX = 0.07
+VELOCITY_RIGHT = 0.07
 
-# use optimistic initial value, so it's ok to set epsilon to 0
-EPSILON = 0
 
-# take an @action at @position and @velocity
-# @return: new position, new velocity, reward (always -1)
+def getNextVelocity(position, velocity, direction):
+    if direction == "Left":
+        return velocity + VELOCITY_ONESTEPSIZE * -1 - GRAVITY_ONESTEPSIZE * math.cos(3 * position)
+    elif direction == "Right":
+        return velocity + VELOCITY_ONESTEPSIZE * 1 - GRAVITY_ONESTEPSIZE * math.cos(3 * position)
+    if direction == "None":
+        return velocity - GRAVITY_ONESTEPSIZE * math.cos(3 * position)
+    
+def getDirection(action):
+    if action == -1:
+        return "Left"
+    elif action == 0:
+        return "None"
+    else:
+        return "Right"
+    
 def step(position, velocity, action):
-    new_velocity = velocity + 0.001 * action - 0.0025 * np.cos(3 * position)
-    new_velocity = min(max(VELOCITY_MIN, new_velocity), VELOCITY_MAX)
-    new_position = position + new_velocity
-    new_position = min(max(POSITION_MIN, new_position), POSITION_MAX)
-    reward = -1.0
-    if new_position == POSITION_MIN:
+    
+    #1step당 action -1, 0, 1로  속력의변화를 -0.001, 0, +0.001의 방향으로 새로운 속력 획득
+    #단 position에 의한 중력 영향을 받아 (-0.0025 * cos(3 * position))의 저항을 받은 최종 속력  
+    direction = getDirection(action)
+    
+    new_velocity = min(max(VELOCITY_LEFT, getNextVelocity(position,velocity,getDirection(action))), VELOCITY_RIGHT)
+    
+    #1step당 구한 새로운 속력으로 다음 포지션 취득
+    new_position = min(max(MAP_LEFT, position + new_velocity), MAP_RIGHT)
+    
+    #만약 왼쪽 position으로 나간다면 속력은 0으로 초기화    
+    if new_position == MAP_LEFT:
         new_velocity = 0.0
+        
+    #1step당 reward는 -1.0
+    reward = REWARD
+            
+    #새로 구한 위치, 속력, 보상  리턴
     return new_position, new_velocity, reward
 
 # wrapper class for state action value function
@@ -145,15 +165,15 @@ class ValueFunction:
         self.weights = np.zeros(max_size)
 
         # 1step 포지션의 scale = 크게 구분한 갯수 / 맵의 실 거리 범위
-        self.position_scale = self.num_of_tilings / (POSITION_MAX - POSITION_MIN)
+        self.position_scale = self.num_of_tilings / (MAP_RIGHT - MAP_LEFT)
         
         # 1step 변화하는 속력의 scale  = 크게 구분한 갯수 / 허용 가능한 속력 범위 
-        self.velocity_scale = self.num_of_tilings / (VELOCITY_MAX - VELOCITY_MIN)
+        self.velocity_scale = self.num_of_tilings / (VELOCITY_RIGHT - VELOCITY_LEFT)
 
     # get indices of active tiles for given state and action
     def get_active_tiles(self, position, velocity, action):
-        # I think positionScale * (position - position_min) would be a good normalization.
-        # However positionScale * position_min is a constant, so it's ok to ignore it.
+        # I think positionScale * (position - MAP_LEFT) would be a good normalization.
+        # However positionScale * MAP_LEFT is a constant, so it's ok to ignore it.
         active_tiles = tiles(self.hash_table, self.num_of_tilings,[self.position_scale * position, self.velocity_scale * velocity], [action])
         return active_tiles
 
@@ -161,22 +181,30 @@ class ValueFunction:
     # value function 획득
     def value(self, position, velocity, action):
         #position이 오른쪽 끝이면 return 0
-        if position == POSITION_MAX:
+        if position == MAP_RIGHT:
             return 0.0
         
         active_tiles = self.get_active_tiles(position, velocity, action)
         
-        #2024개의 weight들의 summation 꼴
+        #active_tiles에 해당하는 인덱스의 값만 summation 
         outputNode = np.sum(self.weights[active_tiles]) 
         return outputNode 
 
-    # learn with given state, action and target
+    # 가중치 업데이트 
     def learn(self, position, velocity, action, target):
+        #위치, 속도, 해당 위치와 속도에서 수행한 action, 그에 따른 보상
+        
+        #사용했던 가중치 리스트 획득
         active_tiles = self.get_active_tiles(position, velocity, action)
+        
+        #사용했던 가중치들의 summation 
         estimation = np.sum(self.weights[active_tiles])
+        
+        #td target - 사용했던 가중치들의 summation 
+        # w의 gradient
         delta = self.step_size * (target - estimation)
         for active_tile in active_tiles:
-            self.weights[active_tile] += delta
+            self.weights[active_tile] += delta # gradient descent
 
     # get # of steps to reach the goal under current state value function
     def cost_to_go(self, position, velocity):
@@ -201,8 +229,7 @@ def get_action(position, velocity, value_function):
     return np.random.choice([action_ for action_, value_ in enumerate(values) if value_ == np.max(values)]) - 1
 
 # semi-gradient n-step Sarsa
-# @valueFunction: state value function to learn
-# @n: # of steps
+# n - step sarsa
 def semi_gradient_n_step_sarsa(value_function, n=1):
     
     # 초기 시작 위치 : -0.6 ~ -0.4 사이의 랜덤 값
@@ -231,36 +258,44 @@ def semi_gradient_n_step_sarsa(value_function, n=1):
 
         if time < T:
             # take current action and go to the new state
+            # 1step 당 새로운 위치, 속력, reward = -1값 얻음
             new_postion, new_velocity, reward = step(current_position, current_velocity, current_action)
             # choose new action
             new_action = get_action(new_postion, new_velocity, value_function)
 
-            # track new state and action
+            # 모든 히스토리 저장
             positions.append(new_postion)
             velocities.append(new_velocity)
             actions.append(new_action)
             rewards.append(reward)
 
-            if new_postion == POSITION_MAX:
+            if new_postion == MAP_RIGHT:
                 T = time
 
         # get the time of the state to update
         update_time = time - n
         if update_time >= 0:
             returns = 0.0
-            # calculate corresponding rewards
+            
+            
+            # culmulative reward 계산
             for t in range(update_time + 1, min(T, update_time + n) + 1):
                 returns += rewards[t]
-            # add estimated state action value to the return
+                
+            # culmulative reward에 state value function을 더함
+            # culmulative reward + gamma * qˆ(S0, A0, w) = td target
             if update_time + n <= T:
+                #culmulative reward += 업데이트 전 state value function 값 = td target
                 returns += value_function.value(positions[update_time + n],
                                                 velocities[update_time + n],
                                                 actions[update_time + n])
-            # update the state value function
-            if positions[update_time] != POSITION_MAX:
+            # value_function의 가중치 w 업데이트
+            if positions[update_time] != MAP_RIGHT:
                 value_function.learn(positions[update_time], velocities[update_time], actions[update_time], returns)
+                
         if update_time == T - 1:
             break
+        
         current_position = new_postion
         current_velocity = new_velocity
         current_action = new_action
@@ -270,15 +305,17 @@ def semi_gradient_n_step_sarsa(value_function, n=1):
 # print learned cost to go
 def print_cost(value_function, episode, ax):
     grid_size = 40
-    positions = np.linspace(POSITION_MIN, POSITION_MAX, grid_size)
-    # positionStep = (POSITION_MAX - POSITION_MIN) / grid_size
-    # positions = np.arange(POSITION_MIN, POSITION_MAX + positionStep, positionStep)
-    # velocityStep = (VELOCITY_MAX - VELOCITY_MIN) / grid_size
-    # velocities = np.arange(VELOCITY_MIN, VELOCITY_MAX + velocityStep, velocityStep)
-    velocities = np.linspace(VELOCITY_MIN, VELOCITY_MAX, grid_size)
+    positions = np.linspace(MAP_LEFT, MAP_RIGHT, grid_size)
+    # positionStep = (MAP_RIGHT - MAP_LEFT) / grid_size
+    # positions = np.arange(MAP_LEFT, MAP_RIGHT + positionStep, positionStep)
+    # velocityStep = (VELOCITY_RIGHT - VELOCITY_LEFT) / grid_size
+    # velocities = np.arange(VELOCITY_LEFT, VELOCITY_RIGHT + velocityStep, velocityStep)
+    velocities = np.linspace(VELOCITY_LEFT, VELOCITY_RIGHT, grid_size)
+    
     axis_x = []
     axis_y = []
     axis_z = []
+    
     for position in positions:
         for velocity in velocities:
             axis_x.append(position)
@@ -291,13 +328,15 @@ def print_cost(value_function, episode, ax):
     ax.set_zlabel('Cost to go')
     ax.set_title('Episode %d' % (episode + 1))
 
-# Figure 10.1, cost to go in a single run
-def figure_10_1():
-    #수행할 에피소드 설정 : 9000번
-    episodes = 9000
-    #에피소드 별 Valuefunction 그래프 0번, 99번 8999번
-    plot_episodes = [0, 99, episodes - 1]
+# simulation1. episode 1, 12, 104, 1000, 9000번의 3d 그래프 저장
+
+def start_simulation1():
     
+    #수행할 에피소드 설정 : 9000번
+   
+    #에피소드 별 Valuefunction 그래프 1번, 12번 104번, 1000번, 9000번
+    plot_episodes = [0, 11, 103, 999, 8999]
+        
     #plot 설정
     fig = plt.figure(figsize=(40, 10))
     axes = [fig.add_subplot(1, len(plot_episodes), i+1, projection='3d') for i in range(len(plot_episodes))]
@@ -308,109 +347,91 @@ def figure_10_1():
     value_function = ValueFunction(alpha, num_of_tilings)
     
     
-    for ep in tqdm(range(episodes)):
+    for ep in tqdm(range(plot_episodes[len(plot_episodes) - 1] + 1)):
         semi_gradient_n_step_sarsa(value_function)
         if ep in plot_episodes:
             print_cost(value_function, ep, axes[plot_episodes.index(ep)])
 
+    print_cost(value_function, ep, axes[plot_episodes.index(ep)])
+
     plt.savefig('../images/figure_10_1.png')
     plt.close()
-
-# Figure 10.2, semi-gradient Sarsa with different alphas
-def figure_10_2():
-    runs = 10
-    episodes = 500
+    
+    
+# simulation2. alpha 값 0.1, 0.2, 0.5 변경하면서 테스트 후 2d 그래프 저장
+def start_simulation2():
+      
+    #value function 초기화
     num_of_tilings = 8
-    alphas = [0.1, 0.2, 0.5]
-
-    steps = np.zeros((len(alphas), episodes))
-    for run in range(runs):
-        value_functions = [ValueFunction(alpha, num_of_tilings) for alpha in alphas]
-        for index in range(len(value_functions)):
-            for episode in tqdm(range(episodes)):
-                step = semi_gradient_n_step_sarsa(value_functions[index])
-                steps[index, episode] += step
-
-    steps /= runs
-
-    for i in range(0, len(alphas)):
-        plt.plot(steps[i], label='alpha = '+str(alphas[i])+'/'+str(num_of_tilings))
+    alphas = [0.1,0.2,0.5]   
+    alphasstepPerEpisods = [] 
+    x = []
+    
+    for i in range(1000):
+        x.append(i + 1)
+            
+    #에피소드 1번당 수행하는 timestep 저장
+    for alpha in alphas:
+        value_function = ValueFunction(alpha, num_of_tilings)
+        stepPerEpisodes = []
+        for i in range(1000):
+            stepPerEpisodes.append(semi_gradient_n_step_sarsa(value_function))
+            
+       
+        alphasstepPerEpisods.append(stepPerEpisodes)
+    
     plt.xlabel('Episode')
-    plt.ylabel('Steps per episode')
-    plt.yscale('log')
-    plt.legend()
-
-    plt.savefig('../images/figure_10_2.png')
-    plt.close()
-
-# Figure 10.3, one-step semi-gradient Sarsa vs multi-step semi-gradient Sarsa
-def figure_10_3():
-    runs = 10
-    episodes = 500
+    plt.ylabel('StepPerEpisode')
+    
+    plt.plot(x, alphasstepPerEpisods[0], label='alpha : 0.1')
+    plt.plot(x, alphasstepPerEpisods[1], label='alpha : 0.2')
+    plt.plot(x, alphasstepPerEpisods[2], label='alpha : 0.5')
+    
+    plt.ylim(100,1000)
+    plt.xlim(0,500)
+    
+    plt.legend(loc='best', ncol=2) 
+    plt.show()
+    
+# simulation3. Bootstrap 1,8 변경하면서 테스트 후 2d 그래프 저장
+def start_simulation3():
+      
+    #value function 초기화
     num_of_tilings = 8
-    alphas = [0.5, 0.3]
-    n_steps = [1, 8]
-
-    steps = np.zeros((len(alphas), episodes))
-    for run in range(runs):
-        value_functions = [ValueFunction(alpha, num_of_tilings) for alpha in alphas]
-        for index in range(len(value_functions)):
-            for episode in tqdm(range(episodes)):
-                step = semi_gradient_n_step_sarsa(value_functions[index], n_steps[index])
-                steps[index, episode] += step
-
-    steps /= runs
-
-    for i in range(0, len(alphas)):
-        plt.plot(steps[i], label='n = %.01f' % (n_steps[i]))
+    alpha = 0.3
+    bootstraps = [1,8]
+    bootstrapsstepPerEpisods = [] 
+    x = []
+    
+    for i in range(1000):
+        x.append(i + 1)
+            
+    #에피소드 1번당 수행하는 timestep 저장
+    for bootstrap in bootstraps:
+        value_function = ValueFunction(alpha, num_of_tilings)
+        stepPerEpisodes = []
+        for i in range(1000):
+            stepPerEpisodes.append(semi_gradient_n_step_sarsa(value_function, bootstrap))
+                  
+        bootstrapsstepPerEpisods.append(stepPerEpisodes)
+    
     plt.xlabel('Episode')
-    plt.ylabel('Steps per episode')
-    plt.yscale('log')
-    plt.legend()
+    plt.ylabel('StepPerEpisode')
+    
+    plt.plot(x, bootstrapsstepPerEpisods[0], label='n : 1')
+    plt.plot(x, bootstrapsstepPerEpisods[1], label='n : 8')
 
-    plt.savefig('../images/figure_10_3.png')
-    plt.close()
+    
+    plt.ylim(100,1000)
+    plt.xlim(0,500)
+    
+    plt.legend(loc='best', ncol=2) 
+    plt.show()
+    
+#start_simulation1()    
+#start_simulation2()
+start_simulation3()
 
-# Figure 10.4, effect of alpha and n on multi-step semi-gradient Sarsa
-def figure_10_4():
-    alphas = np.arange(0.25, 1.75, 0.25)
-    n_steps = np.power(2, np.arange(0, 5))
-    episodes = 50
-    runs = 5
-
-    max_steps = 300
-    steps = np.zeros((len(n_steps), len(alphas)))
-    for run in range(runs):
-        for n_step_index, n_step in enumerate(n_steps):
-            for alpha_index, alpha in enumerate(alphas):
-                if (n_step == 8 and alpha > 1) or \
-                        (n_step == 16 and alpha > 0.75):
-                    # In these cases it won't converge, so ignore them
-                    steps[n_step_index, alpha_index] += max_steps * episodes
-                    continue
-                value_function = ValueFunction(alpha)
-                for episode in tqdm(range(episodes)):
-                    step = semi_gradient_n_step_sarsa(value_function, n_step)
-                    steps[n_step_index, alpha_index] += step
-
-    # average over independent runs and episodes
-    steps /= runs * episodes
-
-    for i in range(0, len(n_steps)):
-        plt.plot(alphas, steps[i, :], label='n = '+str(n_steps[i]))
-    plt.xlabel('alpha * number of tilings(8)')
-    plt.ylabel('Steps per episode')
-    plt.ylim([220, max_steps])
-    plt.legend()
-
-    plt.savefig('D:\\Projects\\MS\\RL Homework\\mountainCar\\figure_10_4.png')
-    plt.close()
-
-if __name__ == '__main__':
-    figure_10_1()
-    #figure_10_2()
-    #figure_10_3()
-    #figure_10_4()
 
 
 
