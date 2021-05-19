81e00b494a5e715c017060c9fe038385f742b166
HANDONGHEE <gksehdmgl@gmail.com>
2021-05-19 21:16:04 +0900

    [수정]OpenAI 사용법 연습

--- RL Study/cartpole_tutorial.py
+++ RL Study/cartpole_tutorial.py
@@ -7,35 +7,23 @@ Created on Tue May 18 10:04:15 2021
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
 
-from JSAnimation.IPython_display import display_animation
 from matplotlib import animation
 from IPython.display import display
 
-def display_frames_as_gif(frames):
-    """
-    Display a list of frames a gif, with controls
-    """
-    
-    plt.figure(figsize=(frames[0].shape[1]/72.0,frames[0].shape[0]/72.0),dpi=72)
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-    
-    def animate(i):
-        patch.set_data(frames[i])
-    
-    anim = animation.FuncAnimation(plt.gcf(),animate, frames=len(frames), internal=50)
-    anim.save('movie_cartpole.mp4')#애니메이션 저장
-    display(display_animation(anim, defaule_mode='loop'))
 
+#frames에 매번 영상 저장
 frames = []
+
+#CartPole 환경 구성
 env = gym.make('CartPole-v0')
+
+#환경 리셋
 observation = env.reset()
 
-for step in range(0,200):
-    #frames 리스트에 rgb칼라 이미지 추가
-    frames.append(env.render(mode='rgb_array'))
+while True:
+    #frames.append()
+    frames.append(env.render())
     action = np.random.choice(2) # 0(왼쪽으로), 1(오른쪽으로)
-    observation, reward, done, info = env,step(action) # action 실행
+    observation, reward, done, info = env.step(action) # action 실행
     
