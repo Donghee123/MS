66e31727d87cf26abe665f96f419731cb6f26c4f
Donghee <gksehdmgl@gmail.com>
2021-08-19 09:57:42 +0900

    [추가]파이토치를 이용한 MLP 실습 1. nn.Module을 상속받아 직접 뉴런 모델 만들어보기 2.GD vs SGD 성능 차이 비교

--- Reinforcement learning/RL Study/ReinforcementLearningAtoZ-master/excercise/part3/hdh_pytorch_mlp.py
+++ Reinforcement learning/RL Study/ReinforcementLearningAtoZ-master/excercise/part3/hdh_pytorch_mlp.py
@@ -10,6 +10,7 @@ import sys; sys.path.append('...')
 import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import numpy as np
 
 from MLP import MultiLayerPerceptron as MLP
 from linear_data import generate_samples
@@ -22,6 +23,73 @@ class MyModel(nn.Module):
         #학습중 업데이트가 되지만, 미분을 활용한 업데이트를 하지 않는 텐서들을 등록할 때
         #혹은 차후에 저장해야하는 텐서값 일때 ex) 입실론 정책의 입실론 값
         self.register_buffer('w',torch.zeros(5))
+
+"""
+Stochastic GD vs GD
+"""
+def train_model(model, opt, loader, epoch, criteria, xs, ys):
+    
+    losses = []
+    
+    for e in range(epoch):
+        for x,y in loader:
+        
+            pred = model(x) #forward 함수를 통해 mlp 모델의 추정치(pred) 계산
+            loss = criteria(pred,y) #pred와 target값 y의 MSE Loss 계산
+        
+            opt.zero_grad()
+            loss.backward() #loss의 파라미터에 대한 편미분 계산
+            opt.step() #경사 하강법을 이용해서 파라미터 계산
+        
+        #전체 데이터셋에 대한 오차 계산
+        pred = model(xs)
+        loss = criteria(pred,ys)
+        loss = loss.detach().numpy() #torch.tensor를 numpy.array로 변환 detach홤수는 최적화에 도움됨
+        losses.append(loss)
+        #another option
+        
+        """
+        with torch.no_grad():
+            pred = mlp(xs)
+            loss = criteria(pred,ys).numpy() 
+            """    
+        
+    
+    return losses
+
+def run_minibatch_fullbatch(num_reps : int, n_sample : int, batch_size : int, epoch : int):
+    criteria = torch.nn.MSELoss() #Loss 함수
+    
+    sgd_losses=[]
+    gd_losses=[]
+    
+    for _ in range(num_reps):
+        mlp = MLP(input_dim=1, output_dim=1, num_neurons=[64],hidden_act='Identity', out_act='Identity')
+        opt_mlp = torch.optim.Adam(params=mlp.parameters(), lr=1e-3)
+        
+        mlp2 = MLP(input_dim=1, output_dim=1, num_neurons=[64],hidden_act='Identity', out_act='Identity')
+        mlp2.load_state_dict(mlp.state_dict()) # mlp파라미터와 mlp2의 파라미터 테스트의 시작점을 통일하기 위해 load_state_dict 사용
+        opt_mlp2 = torch.optim.Adam(params=mlp2.parameters(), lr=1e-3)
+        
+        xs, ys = generate_samples(n_sample)
+        ds = torch.utils.data.TensorDataset(xs,ys)
+        
+        data_loader = torch.utils.data.DataLoader(ds, batch_size=batch_size)
+        full_loader = torch.utils.data.DataLoader(ds, batch_size=n_sample)
+        
+        #SGD = Mini batch
+        sgd_loss = train_model(mlp, opt_mlp, data_loader, epoch, criteria, xs, ys)
+        sgd_losses.append(sgd_loss)
+        
+        #GD = Full batch
+        gd_loss = train_model(mlp2, opt_mlp2, full_loader, epoch, criteria, xs, ys)
+        gd_losses.append(gd_loss)
+        
+    sgd_losses = np.stack(sgd_losses)
+    gd_losses = np.stack(gd_losses)
+    
+    return sgd_losses, gd_losses
+
 
 mm = MyModel()
 
@@ -70,6 +138,50 @@ w = 1.0
 b = 0.5
 xs, ys = generate_samples(512,w=w,b=b)
 
+ds = torch.utils.data.TensorDataset(xs,ys)
+data_loader = torch.utils.data.DataLoader(ds, batch_size=64)
+full_loader = torch.utils.data.DataLoader(ds, batch_size=512)
+
+epoch = 64
+opt = torch.optim.Adam(params=mlp.parameters(), lr=1e-3)
+criteria = torch.nn.MSELoss()
 
 
+sgd_losses, gd_losses = run_minibatch_fullbatch(50,128,32,30)
 
+sgd_loss_mean = np.mean(sgd_losses, axis = 0)
+gd_loss_mean = np.mean(gd_losses, axis = 0)
+
+sgd_loss_std = np.std(sgd_losses, axis = -0)
+gd_loss_std = np.std(gd_losses, axis = -0)
+
+fig, ax = plt.subplots(1,1, figsize=(10,5))
+ax.grid()
+ax.fill_between(x=range(sgd_loss_mean.shape[0]), 
+                y1 = sgd_loss_mean + sgd_loss_std,
+                y2 = sgd_loss_mean - sgd_loss_std,
+                alpha=0.3)
+ax.plot(sgd_loss_mean, label="SGD")
+
+ax.fill_between(x=range(gd_loss_mean.shape[0]), 
+                y1 = gd_loss_mean + gd_loss_std,
+                y2 = gd_loss_mean - gd_loss_std,
+                alpha=0.3)
+ax.plot(gd_loss_mean, label="GD")
+ax.legend()
+
+_ = ax.set_xlabel('epoch')
+_ = ax.set_ylabel('loss')
+_ = ax.set_title('SGD vs GD')
+
+
+""" 
+fig, ax = plt.subplots(1,1, figsize=(10,5))
+ax.grid()
+ax.plot(sgd_losses)
+ax.set_xlabel('opt step')
+ax.set_ylabel('loss')
+ax.set_title('training loss curve')
+"""     
+        
+        
