6a03c5bb3f217f2480731831d514166ded7a220f
Donghee <gksehdmgl@gmail.com>
2021-09-02 10:17:54 +0900

    [추가]DDPG 코드 추가 1개의 node에 action 값이 -2~2로 지정되어있는데 다양한 action들을 뽑을 수 있는지 확인해야함

--- Reinforcement learning/RL Study/ReinforcementLearningAtoZ-master/excercise/part5/DDPG.py
+++ Reinforcement learning/RL Study/ReinforcementLearningAtoZ-master/excercise/part5/DDPG.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from src.part3.MLP import MultiLayerPerceptron as MLP
+from MLP import MultiLayerPerceptron as MLP
 
 
 class OrnsteinUhlenbeckProcess:
@@ -34,6 +34,9 @@ class Actor(nn.Module):
 
     def forward(self, state):
         # Action space of Pendulum v0 is [-2.0, 2.0]
+        # 행동공간을 -2 ~ 2로 제한 시킴.
+        # 출력 레이어의 활성화 함수를 탄젠트하이퍼볼릭을 사용하는것도 괜찮 하지만 BP에서 어려움을 겪음
+        # 그 대신 모델에서 나오는 값을 -2 ~ 2로 clamp 시킴.
         return self.mlp(state).clamp(-2.0, 2.0)
 
 
@@ -41,19 +44,28 @@ class Critic(nn.Module):
 
     def __init__(self):
         super(Critic, self).__init__()
+        
+        #state를 입력으로 받는 네트워크, 임베딩 된 값 -> 64개
         self.state_encoder = MLP(3, 64,
                                  num_neurons=[],
                                  out_act='ReLU')  # single layer model
+        
+        #action를 입력으로 받는 네트워크, 임베딩 된 값 -> 64개
         self.action_encoder = MLP(1, 64,
                                   num_neurons=[],
-                                  out_act='ReLU')  # single layer model
+                                  out_act='ReLU')  # single layer 
+        
+        #sate network, action network의 출력을 합침.
         self.q_estimator = MLP(128, 1,
                                num_neurons=[32],
                                hidden_act='ReLU',
                                out_act='Identity')
 
-    def forward(self, x, a):
-        emb = torch.cat([self.state_encoder(x), self.action_encoder(a)], dim=-1)
+    def forward(self, s, a):
+        #2개의 네트워크를 합침 -> emb
+        #cat -> concatenate를 의미
+        emb = torch.cat([self.state_encoder(s), self.action_encoder(a)], dim=-1)
+        #emb값을 q_estimator에 넣음.
         return self.q_estimator(emb)
 
 
@@ -78,27 +90,31 @@ class DDPG(nn.Module):
         """
 
         super(DDPG, self).__init__()
-        self.critic = critic
-        self.actor = actor
-        self.lr_critic = lr_critic
-        self.lr_actor = lr_actor
+        
+        self.critic = critic #critic 네트워크
+        self.actor = actor #actor 네트워크
+        self.lr_critic = lr_critic #critic의 lr은 보통 다름
+        self.lr_actor = lr_actor  #actor의 lr은 보통 다름
         self.gamma = gamma
 
-        # setup optimizers
+        # setup optimizers Critic
         self.critic_opt = torch.optim.Adam(params=self.critic.parameters(),
                                            lr=lr_critic)
-
+        # setup optimizers Actor
         self.actor_opt = torch.optim.Adam(params=self.actor.parameters(),
                                           lr=lr_actor)
 
-        # setup target networks
+        # setup target Critic networks
         critic_target.load_state_dict(critic.state_dict())
         self.critic_target = critic_target
+        
+        # setup target Actor networks
         actor_target.load_state_dict(actor.state_dict())
         self.actor_target = actor_target
 
+        # Loss 함수는 SmoothL1Loss
         self.criteria = nn.SmoothL1Loss()
-
+        
     def get_action(self, state):
         with torch.no_grad():
             a = self.actor(state)
@@ -109,13 +125,16 @@ class DDPG(nn.Module):
 
         # compute critic loss and update the critic parameters
         with torch.no_grad():
+            #주목할점 actor target network로 action을 진행함
             critic_target = r + self.gamma * self.critic_target(ns, self.actor_target(ns)) * (1 - done)
         critic_loss = self.criteria(self.critic(s, a), critic_target)
 
+        #critic 업데이트
         self.critic_opt.zero_grad()
         critic_loss.backward()
         self.critic_opt.step()
 
+        #actor 업데이트
         # compute actor loss and update the actor parameters
         actor_loss = -self.critic(s, self.actor(s)).mean()  # !!!! Impressively simple
         self.actor_opt.zero_grad()
--- Reinforcement learning/RL Study/ReinforcementLearningAtoZ-master/excercise/part5/DQN.py
+++ Reinforcement learning/RL Study/ReinforcementLearningAtoZ-master/excercise/part5/DQN.py
@@ -86,5 +86,23 @@ class DQN(nn.Module):
         loss.backward()
         self.opt.step()
 
+def prepare_training_inputs(sampled_exps, device='cpu'):
+    states = []
+    actions = []
+    rewards = []
+    next_states = []
+    dones = []
+    for sampled_exp in sampled_exps:
+        states.append(sampled_exp[0])
+        actions.append(sampled_exp[1])
+        rewards.append(sampled_exp[2])
+        next_states.append(sampled_exp[3])
+        dones.append(sampled_exp[4])
 
+    states = torch.cat(states, dim=0).float().to(device)
+    actions = torch.cat(actions, dim=0).to(device)
+    rewards = torch.cat(rewards, dim=0).float().to(device)
+    next_states = torch.cat(next_states, dim=0).float().to(device)
+    dones = torch.cat(dones, dim=0).float().to(device)
+    return states, actions, rewards, next_states, dones
 
