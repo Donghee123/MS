c5f31e531b2ef5afe0d4597614e127dd9c846675
Donghee123 <gksehdmgl@gmail.com>
2021-12-06 06:36:08 +0900

    [수정]pytorch 버전 수정

--- 5G V2X prepare paper/Codes/300_1. MARLspectrumSharingV2X_pytorch 로 변환 적용/MARLspectrumSharingV2X/main_marl_train.py
+++ 5G V2X prepare paper/Codes/300_1. MARLspectrumSharingV2X_pytorch 로 변환 적용/MARLspectrumSharingV2X/main_marl_train.py
@@ -139,7 +139,7 @@ n_RB = n_veh
 env = Environment_marl.Environ(down_lanes, up_lanes, left_lanes, right_lanes, width, height, n_veh, n_neighbor)
 env.new_random_game()  # initialize parameters in env
 
-n_episode = 3000#6000
+n_episode = 9000
 
 n_step_per_episode = int(env.time_slow/env.time_fast) # 1번 episode 수행당 시행할 step 수 100번 수행
 epsi_final = 0.02
@@ -151,13 +151,13 @@ n_episode_test = 100  # test episodes
 
 #10번 업데이트 할때마다 target q-net에 업데이트
 #값이 1이면 target update를 안쓰는거나 마찬가지임
-target_update_interval = 10
+target_update_interval = 15
 
 sampling_only_util = 2000
-lr = 1e-4 * 5
+lr = 1e-4 * 1.3
 batch_size = 256
-gamma = 1.0
-memory_size = 70000
+gamma = 0.98
+memory_size = 100000
 total_eps = 3000
 eps_max = 0.08
 eps_min = 0.01
@@ -312,116 +312,125 @@ if IS_TRAIN:
     scipy.io.savemat(loss_path, {'train_loss': record_loss})
 
 
+IS_TEST = 1
+
 # -------------- Testing --------------
 if IS_TEST:
     print("\nRestoring the model...")
 
-    for i in range(n_veh):
-        for j in range(n_neighbor):
-            model_path = label + '/agent_' + str(i * n_neighbor + j)
-            load_models(agents[i * n_neighbor + j], model_path)
-
-    V2I_rate_list = []
-    V2V_success_list = []
-    V2I_rate_list_rand = []
-    V2V_success_list_rand = []
-    rate_marl = np.zeros([n_episode_test, n_step_per_episode, n_veh, n_neighbor])
-    rate_rand = np.zeros([n_episode_test, n_step_per_episode, n_veh, n_neighbor])
-    demand_marl = env.demand_size * np.ones([n_episode_test, n_step_per_episode+1, n_veh, n_neighbor])
-    demand_rand = env.demand_size * np.ones([n_episode_test, n_step_per_episode+1, n_veh, n_neighbor])
-    power_rand = np.zeros([n_episode_test, n_step_per_episode, n_veh, n_neighbor])
-    for idx_episode in range(n_episode_test):
-        print('----- Episode', idx_episode, '-----')
-
-        env.renew_positions()
-        env.renew_neighbor()
-        env.renew_channel()
-        env.renew_channels_fastfading()
-
-        env.demand = env.demand_size * np.ones((env.n_Veh, env.n_neighbor))
-        env.individual_time_limit = env.time_slow * np.ones((env.n_Veh, env.n_neighbor))
-        env.active_links = np.ones((env.n_Veh, env.n_neighbor), dtype='bool')
-
-        env.demand_rand = env.demand_size * np.ones((env.n_Veh, env.n_neighbor))
-        env.individual_time_limit_rand = env.time_slow * np.ones((env.n_Veh, env.n_neighbor))
-        env.active_links_rand = np.ones((env.n_Veh, env.n_neighbor), dtype='bool')
-
-        V2I_rate_per_episode = []
-        V2I_rate_per_episode_rand = []
-        for test_step in range(n_step_per_episode):
-            # trained models
-            action_all_testing = np.zeros([n_veh, n_neighbor, 2], dtype='int32')
-            for i in range(n_veh):
-                for j in range(n_neighbor):
-                    state_old = get_state(env, [i, j], 1, epsi_final)
-                    state_old = to_tensor(state_old, size=(1, n_input))
-                    
-                    action = predict(agents[i*n_neighbor+j], state_old, epsi_final, True)
-                    action_all_testing[i, j, 0] = action % n_RB  # chosen RB
-                    action_all_testing[i, j, 1] = int(np.floor(action / n_RB))  # power level
-
-            action_temp = action_all_testing.copy()
-            V2I_rate, V2V_success, V2V_rate = env.act_for_testing(action_temp)
-            V2I_rate_per_episode.append(np.sum(V2I_rate))  # sum V2I rate in bps
-            rate_marl[idx_episode, test_step,:,:] = V2V_rate
-            demand_marl[idx_episode, test_step+1,:,:] = env.demand
-
-            # random baseline
-            action_rand = np.zeros([n_veh, n_neighbor, 2], dtype='int32')
-            action_rand[:, :, 0] = np.random.randint(0, n_RB, [n_veh, n_neighbor]) # band
-            action_rand[:, :, 1] = np.random.randint(0, len(env.V2V_power_dB_List), [n_veh, n_neighbor]) # power
-
-            V2I_rate_rand, V2V_success_rand, V2V_rate_rand = env.act_for_testing_rand(action_rand)
-            V2I_rate_per_episode_rand.append(np.sum(V2I_rate_rand))  # sum V2I rate in bps
-            rate_rand[idx_episode, test_step, :, :] = V2V_rate_rand
-            demand_rand[idx_episode, test_step+1,:,:] = env.demand_rand
-            for i in range(n_veh):
-                for j in range(n_neighbor):
-                    power_rand[idx_episode, test_step, i, j] = env.V2V_power_dB_List[int(action_rand[i, j, 1])]
-
-            # update the environment and compute interference
+    #에이전트 그대로 이용
+    
+    
+    
+    mulbytes = [1,2,3,4,5,6]
+    
+    for mulbytes in mulbytes:
+        
+        env.demand_size = int((4 * 190 + 300) * 8 * mulbytes)
+        
+        V2I_rate_list = []
+        V2V_success_list = []
+        V2I_rate_list_rand = []
+        V2V_success_list_rand = []
+        rate_marl = np.zeros([n_episode_test, n_step_per_episode, n_veh, n_neighbor])
+        rate_rand = np.zeros([n_episode_test, n_step_per_episode, n_veh, n_neighbor])
+        demand_marl = env.demand_size * np.ones([n_episode_test, n_step_per_episode+1, n_veh, n_neighbor])
+        demand_rand = env.demand_size * np.ones([n_episode_test, n_step_per_episode+1, n_veh, n_neighbor])
+        power_rand = np.zeros([n_episode_test, n_step_per_episode, n_veh, n_neighbor])
+        
+        for idx_episode in range(n_episode_test):
+            
+            #print('----- Episode', idx_episode, '-----')
+    
+            env.renew_positions()
+            env.renew_neighbor()
+            env.renew_channel()
             env.renew_channels_fastfading()
-            env.Compute_Interference(action_temp)
-
-            if test_step == n_step_per_episode - 1:
-                V2V_success_list.append(V2V_success)
-                V2V_success_list_rand.append(V2V_success_rand)
-
-        V2I_rate_list.append(np.mean(V2I_rate_per_episode))
-        V2I_rate_list_rand.append(np.mean(V2I_rate_per_episode_rand))
-
-        print(round(np.average(V2I_rate_per_episode), 2), 'rand', round(np.average(V2I_rate_per_episode_rand), 2))
-        print(V2V_success_list[idx_episode], 'rand', V2V_success_list_rand[idx_episode])
-
-    print('-------- marl -------------')
-    print('n_veh:', n_veh, ', n_neighbor:', n_neighbor)
-    print('Sum V2I rate:', round(np.average(V2I_rate_list), 2), 'Mbps')
-    print('Pr(V2V success):', round(np.average(V2V_success_list), 4))
-
-    print('-------- random -------------')
-    print('n_veh:', n_veh, ', n_neighbor:', n_neighbor)
-    print('Sum V2I rate:', round(np.average(V2I_rate_list_rand), 2), 'Mbps')
-    print('Pr(V2V success):', round(np.average(V2V_success_list_rand), 4))
-
-    with open("Data.txt", "a") as f:
-        f.write('-------- marl, ' + label + '------\n')
-        f.write('n_veh: ' + str(n_veh) + ', n_neighbor: ' + str(n_neighbor) + '\n')
-        f.write('Sum V2I rate: ' + str(round(np.average(V2I_rate_list), 5)) + ' Mbps\n')
-        f.write('Pr(V2V): ' + str(round(np.average(V2V_success_list), 5)) + '\n')
-        f.write('--------random ------------\n')
-        f.write('Rand Sum V2I rate: ' + str(round(np.average(V2I_rate_list_rand), 5)) + ' Mbps\n')
-        f.write('Rand Pr(V2V): ' + str(round(np.average(V2V_success_list_rand), 5)) + '\n')
-
-    current_dir = os.path.dirname(os.path.realpath(__file__))
-    marl_path = os.path.join(current_dir, "model/" + label + '/rate_marl.mat')
-    scipy.io.savemat(marl_path, {'rate_marl': rate_marl})
-    rand_path = os.path.join(current_dir, "model/" + label + '/rate_rand.mat')
-    scipy.io.savemat(rand_path, {'rate_rand': rate_rand})
-
-    demand_marl_path = os.path.join(current_dir, "model/" + label + '/demand_marl.mat')
-    scipy.io.savemat(demand_marl_path, {'demand_marl': demand_marl})
-    demand_rand_path = os.path.join(current_dir, "model/" + label + '/demand_rand.mat')
-    scipy.io.savemat(demand_rand_path, {'demand_rand': demand_rand})
-
-    power_rand_path = os.path.join(current_dir, "model/" + label + '/power_rand.mat')
-    scipy.io.savemat(power_rand_path, {'power_rand': power_rand})
+    
+            env.demand = env.demand_size * np.ones((env.n_Veh, env.n_neighbor))
+            env.individual_time_limit = env.time_slow * np.ones((env.n_Veh, env.n_neighbor))
+            env.active_links = np.ones((env.n_Veh, env.n_neighbor), dtype='bool')
+    
+            env.demand_rand = env.demand_size * np.ones((env.n_Veh, env.n_neighbor))
+            env.individual_time_limit_rand = env.time_slow * np.ones((env.n_Veh, env.n_neighbor))
+            env.active_links_rand = np.ones((env.n_Veh, env.n_neighbor), dtype='bool')
+    
+            V2I_rate_per_episode = []
+            V2I_rate_per_episode_rand = []
+            for test_step in range(n_step_per_episode):
+                # trained models
+                action_all_testing = np.zeros([n_veh, n_neighbor, 2], dtype='int32')
+                for i in range(n_veh):
+                    for j in range(n_neighbor):
+                        state_old = get_state(env, [i, j], 1, epsi_final)
+                        state_old = to_tensor(state_old, size=(1, n_input))
+                        
+                        action = predict(agents[i*n_neighbor+j], state_old, epsi_final, True)
+                        action_all_testing[i, j, 0] = action % n_RB  # chosen RB
+                        action_all_testing[i, j, 1] = int(np.floor(action / n_RB))  # power level
+    
+                action_temp = action_all_testing.copy()
+                V2I_rate, V2V_success, V2V_rate = env.act_for_testing(action_temp)
+                V2I_rate_per_episode.append(np.sum(V2I_rate))  # sum V2I rate in bps
+                rate_marl[idx_episode, test_step,:,:] = V2V_rate
+                demand_marl[idx_episode, test_step+1,:,:] = env.demand
+    
+                # random baseline
+                action_rand = np.zeros([n_veh, n_neighbor, 2], dtype='int32')
+                action_rand[:, :, 0] = np.random.randint(0, n_RB, [n_veh, n_neighbor]) # band
+                action_rand[:, :, 1] = np.random.randint(0, len(env.V2V_power_dB_List), [n_veh, n_neighbor]) # power
+    
+                V2I_rate_rand, V2V_success_rand, V2V_rate_rand = env.act_for_testing_rand(action_rand)
+                V2I_rate_per_episode_rand.append(np.sum(V2I_rate_rand))  # sum V2I rate in bps
+                rate_rand[idx_episode, test_step, :, :] = V2V_rate_rand
+                demand_rand[idx_episode, test_step+1,:,:] = env.demand_rand
+                for i in range(n_veh):
+                    for j in range(n_neighbor):
+                        power_rand[idx_episode, test_step, i, j] = env.V2V_power_dB_List[int(action_rand[i, j, 1])]
+    
+                # update the environment and compute interference
+                env.renew_channels_fastfading()
+                env.Compute_Interference(action_temp)
+    
+                if test_step == n_step_per_episode - 1:
+                    V2V_success_list.append(V2V_success)
+                    V2V_success_list_rand.append(V2V_success_rand)
+    
+            V2I_rate_list.append(np.mean(V2I_rate_per_episode))
+            V2I_rate_list_rand.append(np.mean(V2I_rate_per_episode_rand))
+    
+            print(round(np.average(V2I_rate_per_episode), 2), 'rand', round(np.average(V2I_rate_per_episode_rand), 2))
+            print(V2V_success_list[idx_episode], 'rand', V2V_success_list_rand[idx_episode])
+    
+        print('-------- marl -------------')
+        print('n_veh:', n_veh, ', n_neighbor:', n_neighbor)
+        print('Sum V2I rate:', round(np.average(V2I_rate_list), 2), 'Mbps')
+        print('Pr(V2V success):', round(np.average(V2V_success_list), 4))
+    
+        print('-------- random -------------')
+        print('n_veh:', n_veh, ', n_neighbor:', n_neighbor)
+        print('Sum V2I rate:', round(np.average(V2I_rate_list_rand), 2), 'Mbps')
+        print('Pr(V2V success):', round(np.average(V2V_success_list_rand), 4))
+    
+        with open("Data.txt", "a") as f:
+            f.write('-------- marl, ' + label + '------\n')
+            f.write('n_veh: ' + str(n_veh) + ', n_neighbor: ' + str(n_neighbor) + '\n')
+            f.write('Sum V2I rate: ' + str(round(np.average(V2I_rate_list), 5)) + ' Mbps\n')
+            f.write('Pr(V2V): ' + str(round(np.average(V2V_success_list), 5)) + '\n')
+            f.write('--------random ------------\n')
+            f.write('Rand Sum V2I rate: ' + str(round(np.average(V2I_rate_list_rand), 5)) + ' Mbps\n')
+            f.write('Rand Pr(V2V): ' + str(round(np.average(V2V_success_list_rand), 5)) + '\n')
+    
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        marl_path = os.path.join(current_dir, "model/" + label + '/rate_marl.mat')
+        scipy.io.savemat(marl_path, {'rate_marl': rate_marl})
+        rand_path = os.path.join(current_dir, "model/" + label + '/rate_rand.mat')
+        scipy.io.savemat(rand_path, {'rate_rand': rate_rand})
+    
+        demand_marl_path = os.path.join(current_dir, "model/" + label + '/demand_marl.mat')
+        scipy.io.savemat(demand_marl_path, {'demand_marl': demand_marl})
+        demand_rand_path = os.path.join(current_dir, "model/" + label + '/demand_rand.mat')
+        scipy.io.savemat(demand_rand_path, {'demand_rand': demand_rand})
+    
+        power_rand_path = os.path.join(current_dir, "model/" + label + '/power_rand.mat')
+        scipy.io.savemat(power_rand_path, {'power_rand': power_rand})
