1dad59ca6396466fedd3aba83fc32974ec9f55e1
Donghee123 <gksehdmgl@gmail.com>
2021-12-22 11:14:02 +0900

    [수정]

--- 5G V2X prepare paper/Codes/18_2. DDPG(2. DDPG(fast campus), action space 확장, power 최소값 옵션으로 추가, test 시뮬레이션으로 직접 학습 추가)/main.py
+++ 5G V2X prepare paper/Codes/18_2. DDPG(2. DDPG(fast campus), action space 확장, power 최소값 옵션으로 추가, test 시뮬레이션으로 직접 학습 추가)/main.py
@@ -72,7 +72,7 @@ def train_V2(args, memory, agent, env):
     selectStep = 0  
     episode = 0
     
-    
+
     
     V2I_Rate_list = np.zeros(totalEpisode)
     V2V_Rate_list = np.zeros(totalEpisode)
@@ -82,6 +82,7 @@ def train_V2(args, memory, agent, env):
         print('===========================')
         print('curEpisode : ', episode)
         
+                    
         #for smple data 
         listOfselRB = []
         listOfselPower = []
@@ -101,15 +102,13 @@ def train_V2(args, memory, agent, env):
         #episode 테스트 시작.
         test_sample = 200
         #print('test game idx:', game_idx)
-        agent.action_all_with_power[:,:,1] = -100.0
-        agent.action_all_with_power[:,:,0] = -1
-        
         for k in range(test_sample):
             action_temp = agent.action_all_with_power.copy()
                       
             for i in range(len(env.vehicles)):
                 selectStep += 1
                 
+                agent.action_all_with_power[i,:,0] = -1
                 sorted_idx = np.argsort(env.individual_time_limit[i,:])      
                 
                 for j in sorted_idx:     
@@ -119,8 +118,6 @@ def train_V2(args, memory, agent, env):
                                       
                     action = agent.get_action(state).cpu().numpy() + ou_noise()[0]
                     selRBIndex, selPowerdBm = GetRB_Power(minPower, action)
-                    
-                    print('RB, Power',[selRBIndex, selPowerdBm])
                     
                     selRBRateList.append(selRBIndex)
                     selPowerRateList.append(selPowerdBm)
@@ -194,8 +191,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch on TORCS with Multi-modal')
 
     parser.add_argument('--mode', default='train_V2', type=str, help='support option: train/test')
-    parser.add_argument('--hidden1', default=512, type=int, help='hidden1 num of first fully connect layer')
-    parser.add_argument('--hidden2', default=256, type=int, help='hidden2 num of second fully connect layer')
+    parser.add_argument('--hidden1', default=256, type=int, help='hidden1 num of first fully connect layer')
+    parser.add_argument('--hidden2', default=128, type=int, help='hidden2 num of second fully connect layer')
     parser.add_argument('--hidden3', default=64, type=int, help='hidden3 num of first fully connect layer')
 
     parser.add_argument('--lr_actor', default=0.005, type=float, help='Actor learning rate')
